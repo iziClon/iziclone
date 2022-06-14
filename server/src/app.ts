3abafdd1638d92797502a4e69319d13d1b4b1207
iziClon <iziclone0@gmail.com>
2022-06-14 22:50:34 +0300

    add first settings and linter

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(5200,()=>{
-    console.log('Server has started')
-})
+app.listen(5200, () => {
+    console.log('Server has started');
+});
