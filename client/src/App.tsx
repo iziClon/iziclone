07b1ff9589d8dfacb3ed367797888c4d7d1f7e17
Serhii-Bondarenko <sergeybondd@gmail.com>
2022-06-16 15:30:27 +0300

    added client config base

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Router } from './router';
+
+const App = () => (
+  <Router />
+);
+
+export default App;
---
+++ client/src/router/Router.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Router = () => (
+  <div>test</div>
+);
+
+export { Router };
