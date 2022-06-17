15ef2aba365f6f64938e752ad9020db88750d249
Natali27M <vns122716@gmail.com>
2022-06-17 16:19:03 +0300

    test

--- server/src/migration/1655464881394-CreateTableCategories.ts
+++ server/src/migration/1655464881394-CreateTableCategories.ts
@@ -1,16 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateTableUsers1645472767107 implements MigrationInterface {
+export class CreateTableCategories1655464881394 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            CREATE TABLE IF NOT EXISTS Users (
+            CREATE TABLE IF NOT EXISTS Categories (
                 id INT PRIMARY KEY AUTO_INCREMENT,
-                firstName VARCHAR(250) NOT NULL,
-                lastName VARCHAR(250) NOT NULL,
-                age INT CHECK (age > 0),
-                phone VARCHAR(250) NOT NULL UNIQUE,
-                email VARCHAR(250) NOT NULL UNIQUE,
-                password VARCHAR(250) NOT NULL,
+                nameCategory VARCHAR(255) NOT NULL,
                 createdAt TIMESTAMP DEFAULT(UTC_TIMESTAMP()) NOT NULL,
                 deletedAt TIMESTAMP
             )
@@ -19,7 +14,7 @@ export class CreateTableUsers1645472767107 implements MigrationInterface {
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DROP TABLE IF EXISTS Users
+            DROP TABLE IF EXISTS Categories
         `);
     }
 }
