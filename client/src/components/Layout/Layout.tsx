4e57ccbcb13b0f8d3265dadd3813a7d3bd4b8bc8
iziClon <iziclone0@gmail.com>
2022-06-19 00:40:48 +0300

    Olena add all elements to header

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,19 +1,20 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import '../../common-styles/index.css';
+import '../../common-styles/normalize.css';
 import { Header } from '../Header/Header';
-import styles from './Layout.module.css';
+import css from './Layout.module.css';
 
 const Layout: FC = () => (
-  <div className={styles.wrapper}>
-    <header className={styles.header}>
+  <div className={css.wrapper}>
+    <header className={css.header}>
       <Header />
     </header>
-    <main className={styles.main}>
+    <main className={css.main}>
       <Outlet />
     </main>
-    <footer className={styles.footer}>Footer</footer>
+    <footer className={css.footer}>Footer</footer>
   </div>
 );
-
 export { Layout };
