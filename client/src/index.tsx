7c59b1d267b07c38ab2fa278822a14865ba8f66d
iziClon <iziclone0@gmail.com>
2022-06-19 13:40:58 +0300

    please don't forget about gap between libraries and own imports (test commit for git available)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import './common-styles/index.css';
 import './common-styles/normalize.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(
