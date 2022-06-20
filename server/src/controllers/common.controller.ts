64b953624c1b3990bac383d666e3e541a2a7d9ab
Natali27M <vns122716@gmail.com>
2022-06-20 16:06:57 +0300

    додала globals.d і post category

--- server/src/controllers/common.controller.ts
+++ server/src/controllers/common.controller.ts
@@ -9,6 +9,11 @@ class CommonController {
         const categories = await commonService.getCategories();
         return res.json(categories);
     }
+
+    public async createCategory(req: Request, res: Response): Promise<Response<ICategory>> {
+        const createdCategory = await commonService.createCategory(req.body);
+        return res.json(createdCategory);
+    }
 }
 
 export const commonController = new CommonController();
