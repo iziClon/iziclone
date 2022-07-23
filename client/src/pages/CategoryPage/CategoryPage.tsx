2f9bd84349a79b198eced54d528d80cd3379a8ea
kniazivna <olenkaknyaz0@gmail.com>
2022-07-23 22:09:09 +0300

    Olena add title to category page

--- client/src/pages/CategoryPage/CategoryPage.tsx
+++ client/src/pages/CategoryPage/CategoryPage.tsx
@@ -4,27 +4,45 @@ import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hook';
 import { getAllProductsByCategory } from '../../store';
 import css from '../HomePage/HomePage.module.css';
+import style from './CategoryPage.module.css';
 import { ProductCardComponent } from '../../components';
 
 const CategoryPage: FC = () => {
   const { productsByCategory } = useAppSelector((state) => state.productsByCategory);
+  const { categories } = useAppSelector((state) => state.categories);
+  console.log(categories);
   const dispatch = useAppDispatch();
 
   const { categoryId } = useParams();
 
   useEffect(() => {
     dispatch(getAllProductsByCategory(+categoryId!));
-  }, []);
+  }, [categoryId]);
 
   return (
-    <div className={css.products}>{productsByCategory.map(
-      (productByCategory) => (
-        <ProductCardComponent
-          key={productByCategory.id}
-          product={productByCategory}
-        />
-      ),
-    )}
+    <div>
+      <div className={style.header}>
+        {categories && categories.map((category) => {
+          let name;
+          if (category.id === Number(categoryId)) {
+            name = category.nameCategory;
+          }
+          return <h1 className={style.title}>{name}</h1>;
+        })}
+        <span className={style.advAmount}>  {productsByCategory.length} оголошення</span>
+      </div>
+      <div className={style.wrapper}>
+        <aside className={style.sidebar}>sidebar</aside>
+        <div className={css.products}>{productsByCategory.map(
+          (productByCategory) => (
+            <ProductCardComponent
+              key={productByCategory.id}
+              product={productByCategory}
+            />
+          ),
+        )}
+        </div>
+      </div>
     </div>
   );
 };
