fa9daa8b454b36f324c0b50da9c8faa7919feab0
TMysiv <tarasmysiv@gmail.com>
2022-07-28 20:33:39 +0300

    add second part of productDetails

--- client/src/pages/ProductDetails/ProductDetails.tsx
+++ client/src/pages/ProductDetails/ProductDetails.tsx
@@ -1,58 +1,192 @@
 /* eslint-disable */
-import React, { FC, useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
-import { productService } from '../../services';
+import React, {FC, useEffect, useState} from 'react';
+import {useLocation, useParams} from 'react-router-dom';
+import {productService} from '../../services';
 
-import { IProduct } from '../../interfaces';
+import {IProduct} from '../../interfaces';
 import css from './ProductDetaids.module.css';
+import 'react-multi-carousel/lib/styles.css';
+import Carousel from 'react-multi-carousel';
+
 
 const ProductDetails: FC = () => {
-  const [product, setProduct] = useState<IProduct>();
-  const { state }:any = useLocation();
+    const [product, setProduct] = useState<IProduct>();
+    const {state}: any = useLocation();
 
-  const { productId } :any = useParams();
+    const {productId}: any = useParams();
 
-  useEffect(() => {
-    if (state) {
-      setProduct(state);
-    }
-    productService.getProductById(productId).then((value) => setProduct(value));
-  }, [productId]);
+    useEffect(() => {
+        if (state) {
+            setProduct(state);
+        }
+        productService.getProductById(productId).then((value) => setProduct(value));
+    }, [productId]);
 
-  return (
-    <div className={css.container}>
 
-      <div className={css.container_header}>
+    const responsiveMain = {
+        desktop: {
+            breakpoint: {max: 3000, min: 1024},
+            items: 2,
+            paritialVisibilityGutter: 40
+        },
+        tablet: {
+            breakpoint: {max: 1024, min: 464},
+            items: 2,
+            paritialVisibilityGutter: 40
 
-        <div className={css.container_image} style={{backgroundImage:`url(${product?.image})`}}>
+        },
+        mobile: {
+            breakpoint: {max: 464, min: 0},
+            items: 2,
+            paritialVisibilityGutter: 40
+
+        },
+    };
+
+    const responsiveSmall = {
+        desktop: {
+            breakpoint: {max: 3000, min: 1024},
+            items: 1,
+            paritialVisibilityGutter: 10
+        },
+        tablet: {
+            breakpoint: {max: 1024, min: 464},
+            items: 1,
+
+        },
+        mobile: {
+            breakpoint: {max: 464, min: 0},
+            items: 1,
+        },
+    };
+
+    return (
+        <div className={css.container}>
+
+            <div className={css.container_header}>
+
+                <div className={css.container_image}>
+                    <Carousel responsive={responsiveMain}
+                              partialVisbile
+                              swipeable={false}
+                              draggable={false}
+                              infinite={true}
+                              ssr={true}>
+                        <div className={css.carousel_image} style={{backgroundImage: `url(${product?.image})`}}></div>
+                        <div className={css.carousel_image} style={{backgroundImage: `url(${product?.image})`}}></div>
+                        <div className={css.carousel_image} style={{backgroundImage: `url(${product?.image})`}}></div>
+                        <div className={css.carousel_image} style={{backgroundImage: `url(${product?.image})`}}></div>
+
+                    </Carousel>
+                </div>
+
+
+                <div className={css.title}>
+
+                    <h2>{product?.title}</h2>
+                    <h4>Опубліковано 26.07.2022</h4>
+                    <h3>{product?.price} ₴</h3>
+
+                    <div style={{marginLeft:"-40px"}}>
+                        <Carousel responsive={responsiveSmall}
+                                  partialVisbile
+                                  infinite={true}
+                                  swipeable={false}
+                                  draggable={false}>
+                            <div className={css.smallSlider}>
+                                <div>
+                                    <img
+                                        src="https://izi.ua/cloud-cgi/static/izi-static/build/images/seller-is-working-1XLK0.svg"
+                                        alt="Saller"
+                                        style={{width: "30px", height: "30px"}}
+                                    />
+                                </div>
+                                <div className={css.smallSlider_box}>
+                                    <h2>Продавець працює</h2>
+                                    <h3>Можна робити замовлення під час війни</h3>
+                                </div>
+                            </div>
+                            <div className={css.smallSlider}>
+                                <div>
+                                    <img
+                                        src="https://izi.ua/cloud-cgi/static/izi-static/build/images/card-3un8_.svg"
+                                        alt="Buy"
+                                        style={{width: "30px", height: "30px"}}
+                                    />
+                                </div>
+                                <div className={css.smallSlider_box}>
+                                    <h2>Купуй безкоштовно</h2>
+                                    <h3>Повернемо гроші,якщо товар не підійде</h3>
+                                </div>
+
+                            </div>
+                            <div className={css.smallSlider}>
+                                <div>
+                                    <img
+                                        src="https://izi.ua/cloud-cgi/static/izi-static/build/images/ukrposhta-z1Ic_.svg"
+                                        alt="Send"
+                                        style={{width: "30px", height: "30px"}}
+                                    />
+                                </div>
+                                <div className={css.smallSlider_box}>
+                                    <h2>Укрпошта 25₴</h2>
+                                    <h3>Спеціальні тарифи для користувачів ІЗІ</h3>
+                                </div>
+
+                            </div>
+                        </Carousel>
+                    </div>
+
+                    <div>
+                        <div className={css.smallSlider}>
+                            <div>
+                                <img
+                                    src="https://izi.ua/cloud-cgi/static/izi-static/build/images/ukrposhta-z1Ic_.svg"
+                                    alt="Send"
+                                    style={{width: "30px", height: "30px"}}
+                                />
+                            </div>
+                            <div className={css.smallSlider_box}>
+                                <h2>Укрпошта 25₴</h2>
+                                <h3>Спеціальні тарифи для користувачів ІЗІ</h3>
+                            </div>
+
+                        </div>
+
+                        <div className={css.smallSlider}>
+                            <div>
+                                <img
+                                    src="https://izi.ua/cloud-cgi/static/izi-static/build/images/ukrposhta-z1Ic_.svg"
+                                    alt="Send"
+                                    style={{width: "30px", height: "30px"}}
+                                />
+                            </div>
+                            <div className={css.smallSlider_box}>
+                                <h2>Укрпошта 25₴</h2>
+                                <h3>Спеціальні тарифи для користувачів ІЗІ</h3>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    <button type={"submit"} className={css.button_buy}>Купити</button>
+                    <button type={"submit"} className={css.button_write}>Написати продавцеві</button>
+
+                </div>
+
+            </div>
+
+            <div className={css.bottom}>
+                <h2>Опис</h2>
+                <h4>{product?.description}</h4>
+            </div>
+
         </div>
 
-        <div className={css.title}>
 
-          <h2>{product?.title}</h2>
-          <h4>Опубліковано 26.07.2022</h4>
-          <h3>{product?.price} ₴</h3>
-
-          <div>
-
-          </div>
-
-          <button type={"submit"} className={css.button_buy}>Купити</button>
-          <button type={"submit"} className={css.button_write}>Написати продавцеві</button>
-
-        </div>
-
-      </div>
-
-      <div className={css.bottom}>
-        <h2>Опис</h2>
-        <h4>{product?.description}</h4>
-      </div>
-
-    </div>
-
-
-  );
+    );
 };
 
-export { ProductDetails };
+export {ProductDetails};
